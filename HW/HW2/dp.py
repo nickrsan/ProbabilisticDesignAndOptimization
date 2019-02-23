caa84c9d03ecc5d995471f62be8b1bd76dde3b66
Nick <ultraayla@gmail.com>
2019-02-22 19:36:50 -0800

    WIP - adding code to run the stages, but lots of pieces still broken

--- HW/HW2/dp.py
+++ HW/HW2/dp.py
@@ -1,31 +1,50 @@
-
+import numpy
 
 MAXIMIZE = max
 MINIMIZE = min
+
+class DecisionVariable(object):
+	def __init__(self, name):
+		self.name = name
 
 
 class DynamicProgram(object):
 	"""
 		This object actually runs the DP - doesn't currently support multiple decision variables or probabilities.
 
-		Currently, this code uses a list-based method for each stage of the DP, but it could likely be sped up significantly
-		by converting this to use numpy instead. This will be a future upgrade.
+		Currently designed to only handle backward DPs
 	"""
 
-	def __init__(self, calculation_function):
+	def __init__(self, calculation_function, timestep_size, time_horizon, selection_constraints=None, decision_variables=None):
 		"""
 
 		:param calculation_function: What function are we using to evaluate? Basically, is this a maximization (benefit)
 		 or minimization (costs) setup. Provide the function object for max or min. Provide the actual `min` or `max functions
 		 (don't run it, just the name) or if convenient, use the shortcuts dp.MINIMIZE or dp.MAXIMIZE
+
+		:param selection_constraints: Is there a minimum value that must be achieved in the selection?
+				If so, this should be a list with the required quantity at each time step
+
+		:param decision_variables: list of DecisionVariable objects
 		"""
 		self.stages = []
+		self.timestep_size = timestep_size
+		self.time_horizon = time_horizon
 
+		# set up decision variables passed in
+		if not decision_variables:
+			self.decision_variables = []
+		else:
+			self.decision_variables = decision_variables
+
+		# Calculation Function
 		self.calculation_function = calculation_function
 
 		if self.calculation_function not in (max, min, MAXIMIZE, MINIMIZE):
 			raise ValueError("Calculation function must be either 'max' or 'min' or one of the aliases in this package of dp.MAXIMIZE or dp.MINIMIZE")
 
+		# make values that we use as bounds in our calculations - when maximizing, use a negative number, and when minimizing, get close to infinity
+		# we use this for any routes through the DP that get excluded
 		if self.calculation_function is max:
 			self.exclusion_value = -1  # just need it to be less
 		elif self.calculation_function is min:
@@ -38,17 +57,62 @@ class DynamicProgram(object):
 		self._index_stages()
 
 	def _index_stages(self):
-		if len(self.stages) > 1: # if there are at least two, then set the next and previous objects on the first and last
+
+		# assigning .number allows us to have constraints on the number of items selected at each stage
+		if len(self.stages) > 1:  # if there are at least two, then set the next and previous objects on the first and last
 			self.stages[0].next = self.stages[1]
+			self.stages[0].number = 0
 			self.stages[-1].previous = self.stages[-2]
+			self.stages[-1].number = len(self.stages) - 1
 
 		for i, stage in enumerate(self.stages[1:-1]):  # for all stages except the first and last, then we set both next and previous
 			self.stages[i].next = self.stages[i+1]
 			self.stages[i].previous = self.stages[i-1]
+			self.stages[i].number = i
+
+	def build_stages(self):
+		"""
+			Make a stage for every timestep
+		:return:
+		"""
+		for stage_id in range(self.time_horizon, self.timestep_size):
+			self.add_stage()
+
+	def run(self):
+
+		# build a matrix where everything is 0  - need to figure out what the size of the x axis is
+		matrix = numpy.zeros((int(self.time_horizon/self.timestep_size), ))
+
+		for stage in range(self.time_horizon, self.timestep_size):
+			for index, row in enumerate(matrix):
+				# if index >= required[year]:
+				#	needed_now = index - required[year]
+				matrix[index][year] = pv_cost(index, year)
+
+		matrix_array = numpy.array(matrix)  # make it a numpy array so we can easily take a vertical slice
+
+		stages = []
+		for year in range(num_years):
+			cost_list = matrix_array[1:,
+						year]  # pull the column out of the matrix corresponding to this year - remove the 0 value first row (should look into how this is getting there)
+			year_stage = Stage(name="Year {}".format(year), cost_benefit_list=list(cost_list), calculation_function=min,
+							   selection_constraints=required)
+			year_stage.max_selections = needed_trucks
+			year_stage.number = year
+			stages.append(year_stage)
+
+		for index, stage in enumerate(stages):  # make the relationships now
+			if index > 0:
+				stages[index].previous = stages[index - 1]
+			if index + 1 < len(stages):  # if it's not the last one
+				stages[index].next = stages[index + 1]
+
+		stages[-1].optimize()
+		stages[0].get_optimal_values()
 
 
 class Stage(object):
-	def __init__(self, name, cost_benefit_list, parent_dp, max_selections=7, previous=None, next=None, selection_constraints=None):
+	def __init__(self, name, cost_benefit_list, parent_dp, max_selections=7, previous=None, next=None):
 		"""
 
 		:param name:
@@ -56,7 +120,6 @@ class Stage(object):
 		:param max_selections: How many total items are we selecting?
 		:param previous: The previous stage, if one exists
 		:param next: The next stage, if one exists
-		:param selection_constraints: Is there a minimum value that must be achieved in the selection? If so, this should be a list with the required quantity at each time step
 		"""
 		self.name = name
 		self.parent_dp = parent_dp
@@ -65,20 +128,19 @@ class Stage(object):
 		self.next = next
 		self.previous = previous
 		self.matrix = None  # this will be created from the parameters when .optimize is run
-		self.selection_constraints = selection_constraints
 		self.number = None
 
 	def optimize(self, prior=None):
 
-		if self.selection_constraints and self.number is None:
+		if self.parent_dp.selection_constraints and self.number is None:
 			raise ValueError("Stage number(.number) must be identified in sequence in order to use selection constraints")
 
 		# first, we need to get the data from the prior stage - if nothing was passed in, we're on the first step and can skip some things
 		if prior is None:
 			self.pass_data = self.cost_benefit_list
-			if self.selection_constraints:  # then we have selections constraints
+			if self.parent_dp.selection_constraints:  # then we have selections constraints
 				for row_index, row_value in enumerate(self.pass_data):
-					if row_index >= len(self.pass_data) - self.selection_constraints[self.number]:
+					if row_index >= len(self.pass_data) - self.parent_dp.selection_constraints[self.number]:
 						self.pass_data[row_index] = self.parent_dp.exclusion_value
 		else:
 			# we're on a stage after the first stage
@@ -94,9 +156,9 @@ class Stage(object):
 				self.matrix[index][index+1] = value
 
 			# set up maximum selection constraints based on what will have been required previously
-			if self.selection_constraints:  # then we have selections constraints
+			if self.parent_dp.selection_constraints:  # then we have selections constraints
 				for row_index, row_value in enumerate(self.matrix):
-						if row_index >= len(self.matrix) - self.selection_constraints[self.number]:  # if the row is less than the constrained amount for this stage
+						if row_index >= len(self.matrix) - self.parent_dp.selection_constraints[self.number]:  # if the row is less than the constrained amount for this stage
 							for column_index, column_value in enumerate(self.matrix[row_index]):
 								self.matrix[row_index][column_index] = self.parent_dp.exclusion_value
 
